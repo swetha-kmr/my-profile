8dcea6bc7f29c322539e49ddae958a2e9940a7a7
swetha-kmr <swethasenthilkmr2@gmail.com>
2025-02-11 12:10:09 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,8 +8,9 @@ const HeaderSocials = () => {
     
     <div className="header__socials">
 
-        <a href="" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+       
         <a href="https://github.com/swetha-kmr" target="_blank" rel='noreferrer'><BsGithub/></a>
+       <a href="www.linkedin.com/in/swetha-senthil-kumar-1b9053269" target="_blank" rel='noreferrer'><BsLinkedin/></a>
         
     </div>
   )
